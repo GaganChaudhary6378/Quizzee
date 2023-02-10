e6b88e0bf49899c6cf6fd977b5d429012861ea61
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-02-10 18:22:29 +0530

    added questions and options

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,53 @@
 import React from "react";
 import Start from "./Start";
 import Questions from "./Questions";
+import { nanoid } from "../nanoid/index.browser";
 
 export default function App() {
   const [ques, setQues] = React.useState([]);
-  const [start,setStart]=React.useState(true)
+  const [start, setStart] = React.useState(false);
+  const [count, setCount] = React.useState(0);
 
   React.useEffect(() => {
     fetch(
       "https://opentdb.com/api.php?amount=5&category=18&difficulty=hard&type=multiple"
     )
       .then((res) => res.json())
-      .then((data) => setQues(data.results));
-  }, []);
+      .then((data) => {
+        let arr = [];
+        data.results.map((item) => {
+          arr.push({id:nanoid(),answers:[item.incorrect_answers,item.correct_answer],question:item.question});
+        });
+        setQues(arr);
+      });
+  }, [count]);
 
-  function startKaro(){
-    setStart(prevState => !prevState)
+
+  const questionElement = ques ? ques.map(question =>{
+    return(
+      <Questions
+       key={question.id}
+       options={question.answers}
+       q={question.question}
+       id={question.id}
+      />
+    )
+   }) : []
+
+  function startKaro() {
+    setStart((prevState) => !prevState);
   }
-
 
   return (
     <main>
-      {!start ? (
+      {start ? (
         <div>
           <img className="img1" src="blobs.png" />
-          <div className="question-container">
-            <Questions/>    
-          </div>
+          <div className="question-container">{questionElement}</div>
           <img className="img2" src="blob5.png" />
         </div>
       ) : (
-        <Start startFunc={() => startKaro}/>
+        <Start startFunc={() => startKaro} />
       )}
     </main>
   );
--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { nanoid } from "../nanoid/index.browser";
 
 export default function Questions(props) {
-  return (
-    <div>
-      <h3 className="question-el">{props.value}</h3>
-    </div>
+    let answers = props.options[0];
+    answers.push(props.options[1])
+    console.log(answers)
+    const answerElements=answers.map((items) => {
+        return (
+            <button className="btn-el">{items}</button>
+        )
+    })
+    return (
+        <div>
+            <h3 className="question-el">{props.q}</h3>
+            <div className="option-el">{answerElements}</div>
+        </div>
   );
 }
