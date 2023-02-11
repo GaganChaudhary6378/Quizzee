181e7b1a8e853b745d8e760ad23a9ff97bf0d1bc
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-02-11 18:08:22 +0530

    last step is left

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ export default function App() {
   const [ques, setQues] = React.useState([]);
   const [start, setStart] = React.useState(false);
   const [count, setCount] = React.useState(0);
+  const [isHeld,setHeld]=React.useState(false)
+  const [correct,setCorrect]=React.useState(0)
 
   React.useEffect(() => {
     fetch(
@@ -15,10 +17,12 @@ export default function App() {
     )
       .then((res) => res.json())
       .then((data) => {
+        console.log(data)
         let arr = [];
         data.results.map((item) => {
-          arr.push({id:nanoid(),answers:[item.incorrect_answers,item.correct_answer],question:item.question});
+          arr.push({id:nanoid(),answers:[item.incorrect_answers,item.correct_answer],question:item.question,correct:item.correct_answer,selected:null,isHeld:false});
         });
+        // console.log(data.results)
         setQues(arr);
       });
   }, [count]);
@@ -29,16 +33,53 @@ export default function App() {
       <Questions
        key={question.id}
        options={question.answers}
-       q={question.question}
+       q={question}
        id={question.id}
+    //    isHeld={question.isHeld}
+       isHold={isHold}
       />
     )
    }) : []
-
+//    console.log(ques
   function startKaro() {
     setStart((prevState) => !prevState);
   }
 
+  function handleCheck(){
+    let selected=true;
+    ques.forEach(question => {
+        if(question.selected==null){
+            selected=false
+            return 
+        }
+        if(!selected){
+            return 
+        }
+        setQues(ques => ques.map(question => {
+            return {...question,isHeld:true}
+        }))
+    })
+    setHeld(true)
+    let correct=0
+    ques.forEach(question => {
+        if(question.correct===question.selected){
+            correct+=1;
+        }
+    })
+    setCorrect(correct)
+  }
+//   console.log(correct)
+  function isHold(id,answer){
+    // console.log(id); 
+    // setQues(ques => ques.map(question =>{
+    //     return id === answer.id ? {...question, isHeld: !question.isHeld} : question
+    //   }))
+    //   console.log(ques)
+    setQues(ques => ques.map(question =>{
+        return question.id === id ? {...question, selected: answer} : question
+      }))
+  }
+  console.log(ques)
   return (
     <main>
       {start ? (
@@ -46,7 +87,7 @@ export default function App() {
           <img className="img1" src="blobs.png" />
           <div className="question-container">{questionElement}</div>
           <img className="img2" src="blob5.png" />
-          <Check/>
+          <Check />
         </div>
       ) : (
         <Start startFunc={() => startKaro} />
--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,22 +1,58 @@
 import React from "react";
+import App from "./App";
 import { nanoid } from "../nanoid/index.browser";
 
 export default function Questions(props) {
+  const [finalArr, setNewArr] = React.useState([]);
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+  React.useEffect(() => {
     let answers = props.options[0];
-    answers.push(props.options[1])
-    console.log(answers)
-    const answerElements=answers.map((items) => {
-        return (
-            <button className="btn-el">{items}</button>
-        )
-    })
-    return (
-        <div>
-            <h3 className="question-el">{props.q}</h3>
-            <div className="option-el">{answerElements}</div>
-            <br></br>
-            <hr className="pagebr-el"></hr>
+    answers.push(props.options[1]);
+    let newArr = [];
+    let temp = answers.map((items) => {
+      newArr.push({ value: items });
+    });
+    setNewArr(newArr);
+  },[]);
 
-        </div>
+  const answerElements = finalArr.map((items) => {
+    let id = null;
+    if (props.q.isHeld) {
+      if (props.q.correct === items) {
+        id = "correct";
+      } else if (props.q.selected === items) {
+        id = "incorrect";
+      } else {
+        id = "not-selected";
+      }
+    }
+    return (
+      <button
+        key={nanoid()}
+        id={id}
+        className={items === props.q.selected ? "answer-chosen" : "btn-el"}
+        onClick={() => handleClick(items)}
+      >
+        {items.value}
+      </button>
+    );
+  });
+  function handleClick(item) {
+    if (props.q.checked) {
+      return;
+    }
+    props.isHold(props.id, item);
+  }
+
+//   console.log(newArr);
+  return (
+    <div>
+      <h3 className="question-el">{props.q.question}</h3>
+      <div className="option-el">{answerElements}</div>
+      <br></br>
+      <hr className="pagebr-el"></hr>
+    </div>
   );
 }
